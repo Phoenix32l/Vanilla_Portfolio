8a55b22f61667a64969d8943f919feb152924d33
Abdullah Umair <zohaib.u123@gmail.com>
2024-03-24 16:08:37 -0400

    Test

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+const counter = document.querySelector(".counter-number");
+async function updateCounter(){
+    let response=  await fetch("https://lz32rsc2d7qjt2i2bkrmlh5j6a0volpl.lambda-url.us-east-1.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = `Views:${data}`;
+}
+
+updateCounter();
